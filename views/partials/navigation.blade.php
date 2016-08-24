e4e74eb465fd2445242726d52918d251b186b1b6
Ivan <zambanello@upgrade4.it>
2016-08-24 15:44:34 +0200

    Revert "test"
    This reverts commit 29233cf369b64656fee3cf12e9a233bc55cbe541.

--- views/partials/navigation.blade.php
+++ views/partials/navigation.blade.php
@@ -1,12 +1,3 @@
-
-<div class="cbp-af-header">
-	<div class="cbp-af-inner">
-		<h1>Fixed</h1>
-		<nav>
-			<a href="#">Broccoli</a>
-			<a href="#">Almonds</a>
-			<a href="#">Pears</a>
-			<a href="#">Oranges</a>
-		</nav>
-	</div>
-</div>
+<section class="navigation">
+	Navigation Here
+</section>
