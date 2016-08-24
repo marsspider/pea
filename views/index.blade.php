29233cf369b64656fee3cf12e9a233bc55cbe541
Ivan <zambanello@upgrade4.it>
2016-08-24 15:34:13 +0200

    test

--- views/index.blade.php
+++ views/index.blade.php
@@ -6,22 +6,11 @@
 
 @section('content')
 	<div class="container">
-		<div class="row">
-			<div class="col-xs-12 splash">
-				<img src="/assets/img/PeaLogo.png" alt="" />
-				<span>Pico Framework for PHP with Multilanguage, Routing e Blade Templates</span>
-			</div>
-		</div>
-		<div class="row">
-			@if($data != '')
-				<div class="col-xs-12 splash">
-					You are entering with path: {{ $data }}
-				</div>
-			@endif
-		</div>
+
 	</div>
 @stop
 
 @section('scripts')
+	<script src="/assets/js/classie.js"></script>
 	<script src="/assets/js/scripts.js"></script>
 @endsection
--- views/partials/navigation.blade.php
+++ views/partials/navigation.blade.php
@@ -1,3 +1,12 @@
-<section class="navigation">
-	Navigation Here
-</section>
+
+<div class="cbp-af-header">
+	<div class="cbp-af-inner">
+		<h1>Fixed</h1>
+		<nav>
+			<a href="#">Broccoli</a>
+			<a href="#">Almonds</a>
+			<a href="#">Pears</a>
+			<a href="#">Oranges</a>
+		</nav>
+	</div>
+</div>
