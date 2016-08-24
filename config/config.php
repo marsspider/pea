ace597fece6e15ebddb8f0f41a000910be0d2f25
Ivan <zambanello@upgrade4.it>
2016-08-24 15:10:46 +0200

    Cache Fix

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
 
-	define("SITE_NAME", 'Site Name');
+	define("SITE_NAME", 'Pea Pico Framework');
 	define("VIEWS", BASE_PATH.'/views');
 	define("CACHE", BASE_PATH.'/cache');
--- views/index.blade.php
+++ views/index.blade.php
@@ -9,13 +9,15 @@
 		<div class="row">
 			<div class="col-xs-12 splash">
 				<img src="/assets/img/PeaLogo.png" alt="" />
-				<span>Pico Framework for PHP with Multilanguage, Routing e BladeTemplate</span>
+				<span>Pico Framework for PHP with Multilanguage, Routing e Blade Templates</span>
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-xs-12 splash">
-				You are entering with path: {{ $data }}
-			</div>
+			@if($data != '')
+				<div class="col-xs-12 splash">
+					You are entering with path: {{ $data }}
+				</div>
+			@endif
 		</div>
 	</div>
 @stop
